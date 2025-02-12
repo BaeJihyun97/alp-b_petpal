0b31dd2f199f11fe8eaacf29c90084f34ba667b3
BaeJihyun97 <baejihyun970519@gmail.com>
2025-02-12 17:10:55 +0900

    front init

---
+++ frontend/src/components/UserFormCom.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+      <input v-model="formData.userId" placeholder="User ID" />
+      <input v-model="formData.password" placeholder="Password" type="password" />
+      <input v-model="formData.email" placeholder="Email" type="email" />
+      <input v-model="formData.name" placeholder="Name" />
+      <input v-model="formData.phoneNumber" placeholder="Phone Number" />
+      <button @click="submitForm">Submit</button>
+    </div>
+  </template>
+  
+  <script>
+  import UserForm from '@/composables/UserForm';
+  
+  export default {
+    setup() {
+      return UserForm();
+    }
+  };
+  </script>
